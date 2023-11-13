f422b1ad2355fc6624caa355bf28ca0b876d82fc
Student_DT <Student_DT@HUEPAR-11>
2023-11-13 15:16:53 +0600

    Добавьте файлы проекта.

---
+++ Mai_lab_06_11_23.c
@@ -0,0 +1,101 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+/*
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+
+typedef enum {
+    WaitingNumber,
+    ReadingNumber,
+    BadNumber
+} State;
+
+
+bool isDelimiter(char symbol) {
+    // return isdigit(symbol) && (symbol - '0') < 8;
+    return symbol == ' ' || symbol == '\n' || symbol == '\t';
+}
+
+
+int main() {
+    State state = WaitingNumber;
+
+    char current;
+    long long number = 0;
+    long long count = 0;
+
+    while (scanf("%c", &current) != EOF) {
+        switch (state) {
+        case ReadingNumber: {
+            if (isdigit(current)) {
+                int currentNumber = current - '0';
+                if (currentNumber >= 8) {
+                    state = BadNumber;
+                    number = 0;
+                    break;
+                }
+
+                number *= 8;
+                number += currentNumber;
+                state = ReadingNumber;
+
+                if (number > 1000) {
+                    state = BadNumber;
+                    number = 0;
+                    break;
+                }
+                break;
+            }
+
+            if (isDelimiter(current)) {
+                if (10 <= number && number < 1000) {
+                    count += 1;
+                    number = 0;
+                }
+                state = WaitingNumber;
+                break;
+            }
+
+            state = BadNumber;
+            number = 0;
+
+            break;
+        }
+        case BadNumber: {
+            if (isDelimiter(current)) {
+                state = WaitingNumber;
+            }
+            break;
+        }
+        case WaitingNumber: {
+            if (isdigit(current)) {
+                int currentNumber = current - '0';
+                if (currentNumber >= 8) {
+                    state = BadNumber;
+                    number = 0;
+                    break;
+                }
+
+                number = currentNumber;
+                state = ReadingNumber;
+
+                break;
+            }
+            state = BadNumber;
+            break;
+        }
+        }
+    }
+
+    if (state == ReadingNumber) {
+        if (10 <= number && number < 1000) {
+            count += 1;
+        }
+    }
+
+    printf("Количество восьмиричных чисел, находящихся в диапазоне от 10 до 1000: %lli\n", count);
+}
+
+*/
---
+++ Mai_lab_13_11_23.c
@@ -0,0 +1,101 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+
+typedef struct {
+	int value;
+	int length;
+} tSet;
+
+
+bool set_equals(const tSet* set, const tSet* set2) {
+	return set->length == set2->length && set->value == set2->value;
+}
+
+tSet set_by_index(const tSet set, int index, bool value) {
+	tSet set2 = set;
+
+	set2.value &= ~(1 << index);
+	set2.value |= ((1 ? value : 0) << index);
+
+	return set2;
+}
+
+
+
+typedef struct {
+	tSet symbols;
+	bool badWord;
+} tWord;
+
+tWord word_empty() {
+	tWord s = { 0, 0, 0 };
+	return s;
+}
+
+bool words_good_and_equals(const tWord* a, const tWord* b) {
+	if (a->badWord)
+		return false;
+
+	if (b->badWord)
+		return false;
+
+	return set_equals(&(a->symbols), &(b->symbols));
+}
+
+
+
+bool isDelimiter(char symbol) {
+	return symbol == ' ' || symbol == '\n' || symbol == '\t';
+}
+
+
+
+int main() {
+	tWord lastWord = { 0, 0, 0 };
+	tWord word = { 0, 0, 0 };
+
+	long long count = 0;
+
+	char current;
+	bool previousCharIsDelimiter = false;
+
+	while (scanf("%c", &current) != EOF)
+	{
+		if (isDelimiter(current)) {
+			if (previousCharIsDelimiter) {
+				continue;
+			}
+
+			if (words_good_and_equals(&lastWord, &word)) {
+				count++;
+			}
+
+			lastWord = word;
+			word = word_empty();
+			previousCharIsDelimiter = true;
+			continue;
+		}
+
+		previousCharIsDelimiter = false;
+
+		if (!isalpha(current)) {
+			word.badWord = true;
+			continue;
+		}
+
+		if (!word.badWord) {
+			word.symbols = set_by_index(word.symbols, current - 'a', true);
+		}
+	}
+
+	if (words_good_and_equals(&lastWord, &word)) {
+		count++;
+	}
+
+
+	printf("Result: %lli\n", count);
+}
